22cea916f27f583b7b73743048448be923f7687c
taitus <sebastia.roig@gmail.com>
2022-12-23 12:13:35 +0100

    Modify admin layout to only manage tenants and admins
    We only want to render the account link and login items in the header.
    And we want only render the Multitenancy and Administrators sections in
    the admin sidebar.
    
    We include the administrators management so it's possible to give
    permissions to other users to manage tenants.
    
    In order to restrict access to other sections by typing the URL or
    following a link, we're only enabling the rest of the routes when we
    aren't in the multitenancy management mode.

---
+++ spec/system/multitenancy_management_mode_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe "Multitenancy management mode", :admin do
+  before { allow(Rails.application.config).to receive(:multitenancy_management_mode).and_return(true) }
+
+  scenario "renders expected content for multitenancy manage mode in admin section" do
+    visit admin_root_path
+
+    within ".top-links" do
+      expect(page).not_to have_content "Go back to CONSUL"
+    end
+
+    within ".top-bar" do
+      expect(page).to have_css "li", count: 2
+      expect(page).to have_content "My account"
+      expect(page).to have_content "Sign out"
+    end
+
+    within "#admin_menu" do
+      expect(page).to have_content "Multitenancy"
+      expect(page).to have_content "Administrators"
+      expect(page).to have_css "li", count: 2
+    end
+  end
+end
