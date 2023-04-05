6ee8173581b6e311d551c44c7b6a2f30135df344
taitus <sebastia.roig@gmail.com>
2023-04-05 17:00:06 +0200

    Allow define maximum_attemps and unlock_in

--- app/models/custom/user.rb
+++ app/models/custom/user.rb
@@ -2,4 +2,12 @@ require_dependency Rails.root.join("app", "models", "user").to_s
 
 class User
   devise :lockable if Rails.application.config.devise_lockable
+
+  def self.maximum_attempts
+    (Tenant.current_secrets.dig(:security, :lockable, :maximum_attempts) || 20).to_i
+  end
+
+  def self.unlock_in
+    (Tenant.current_secrets.dig(:security, :lockable, :unlock_in) || 1).to_i.hours
+  end
 end
--- spec/controllers/custom/users/sessions_controller_spec.rb
+++ spec/controllers/custom/users/sessions_controller_spec.rb
@@ -9,11 +9,13 @@ describe Users::SessionsController do
     describe "Devise lock" do
       context "when devise sign in maximum_attempts reached", :with_frozen_time do
         it "locks the user account and sends an email to the account with an unlock link" do
-          user.update(failed_attempts: 19)
+          allow(Tenant.current_secrets).to receive(:security).and_return({ lockable: { maximum_attempts: 1}})
+
           expect do
             post :create, params: { user: { login: "citizen@consul.org", password: "wrongpassword" }}
           end.to change { user.reload.failed_attempts }.by(1)
             .and change { user.reload.locked_at }.from(nil).to(Time.current)
+
           expect(ActionMailer::Base.deliveries.count).to eq(1)
           body = ActionMailer::Base.deliveries.last.body
           expect(body).to have_content "Tu cuenta ha sido bloqueada"
