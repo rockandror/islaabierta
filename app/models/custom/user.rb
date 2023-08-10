89115bf121c7ea5f56a3a51c99716083560439bd
taitus <sebastia.roig@gmail.com>
2023-08-10 14:28:10 +0200

    Add password complexity

--- app/models/custom/user.rb
+++ app/models/custom/user.rb
@@ -10,4 +10,12 @@ class User
   def self.unlock_in
     (Tenant.current_secrets.dig(:security, :lockable, :unlock_in) || 1).to_i.hours
   end
+
+  def self.password_complexity
+    if Tenant.current_secrets.dig(:security, :password_complexity)
+      { digit: 1, lower: 1, symbol: 0, upper: 1 }
+    else
+      { digit: 0, lower: 0, symbol: 0, upper: 0 }
+    end
+  end
 end
