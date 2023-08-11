57dcef8f9411f4cb17f751cdbd99057e3a71ae08
taitus <sebastia.roig@gmail.com>
2023-08-11 11:05:43 +0200

    Allow setting git_http_username and git_http_password
    In order to allow deploying in Isla Abierta servers, we need allow setting
    these configuration parameters.

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,6 +14,8 @@ set :deploy_to, deploysecret(:deploy_to)
 set :ssh_options, port: deploysecret(:ssh_port)
 
 set :repo_url, "https://git.nubarron.teide.int/shs-consul/isla-abierta"
+set :git_http_username, deploysecret(:git_http_username)
+set :git_http_password, deploysecret(:git_http_password)
 
 set :revision, `git rev-parse --short #{fetch(:branch)}`.strip
 
