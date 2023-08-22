79e5894cf2f6d6a79c9e2584ebd30253a44364c6
taitus <sebastia.roig@gmail.com>
2023-08-22 13:14:54 +0200

    Update preproduction log_level to :warn
    In order to reduce the preproduction.log size in preproduction
    environment, we are update the log_level from :debug to :warn

--- config/environments/custom/preproduction.rb
+++ config/environments/custom/preproduction.rb
@@ -1,4 +1,8 @@
 Rails.application.configure do
   # Overwrite settings for the preproduction environment or add your own
   # custom settings for this environment.
+
+  # Use the lowest log level to ensure availability of diagnostic information
+  # when problems arise.
+  config.log_level = :warn
 end
