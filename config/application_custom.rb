1d0b6265af912e43e6364e1c9bd3b0e71908438d
Javi Martín <javim@elretirao.net>
2024-03-06 19:36:19 +0100

    Remove custom code already in Consul Democracy 2.1.0
    These changes were included in Consul Democracy in commits 87fc3c572,
    7c771b28b, d54a5c2ae, b7073691f, a1955531e, 873ec84b5, 84b88c0ec and
    622f351db.

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -8,6 +8,5 @@ module Consul
 
     # Set to true to enable database tables auditing
     config.auditing_enabled = Rails.application.secrets.auditing_enabled
-    config.devise_lockable = Rails.application.secrets.devise_lockable
   end
 end
