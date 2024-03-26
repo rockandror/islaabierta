66f9062e5435fe2f7f65a211faf713fcc4a608ff
Javi Martín <javim@elretirao.net>
2024-03-26 14:42:56 +0100

    Simplify using helper methods in custom components
    Just like it was done in Consul Democracy components in commit
    1d955b7a2.

--- app/components/custom/admin/audits/row_component.rb
+++ app/components/custom/admin/audits/row_component.rb
@@ -2,7 +2,7 @@ class Admin::Audits::RowComponent < ApplicationComponent
   with_collection_parameter :audit
   attr_reader :audit
 
-  delegate :field_name, :audit_value, :new_value, :old_value, to: :helpers
+  use_helpers :field_name, :audit_value, :new_value, :old_value
 
   def initialize(audit:)
     @audit = audit
